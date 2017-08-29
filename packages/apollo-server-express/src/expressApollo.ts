d927e3e20862e710a2b5ee30f1a0517311b582ca
Zackery Leman <zleman1593@gmail.com>
2017-08-29 12:20:37 -0700

    more clean up

--- packages/apollo-server-express/src/expressApollo.ts
+++ packages/apollo-server-express/src/expressApollo.ts
@@ -1,12 +1,7 @@
 import * as express from 'express';
 import * as url from 'url';
-<<<<<<< HEAD:packages/graphql-server-express/src/expressApollo.ts
-import { GraphQLOptions, HttpQueryError, runHttpQuery } from 'graphql-server-core-propagate-errors';
-import * as GraphiQL from 'graphql-server-module-graphiql';
-=======
 import { GraphQLOptions, HttpQueryError, runHttpQuery } from 'apollo-server-core';
 import * as GraphiQL from 'apollo-server-module-graphiql';
->>>>>>> upstream/master:packages/apollo-server-express/src/expressApollo.ts
 
 export interface ExpressGraphQLOptionsFunction {
   (req?: express.Request, res?: express.Response): GraphQLOptions | Promise<GraphQLOptions>;
